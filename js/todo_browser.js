89260e2cbd0c89d7e77dffc9d96fc1375487c4a5
Marina Khadieva <utabond89@gmail.com>
2022-03-15 12:43:45 +0300

    Работает удаление

--- js/todo_browser.js
+++ js/todo_browser.js
@@ -1,12 +1,13 @@
 import { TODO_UI } from './view.js';
-let BUTTONS_DEL = document.querySelectorAll('todo__btn--del');
-let INPUTS_CHECKED = document.querySelectorAll('option:checked');
+let BUTTONS_DEL = document.querySelectorAll('.todo__btn--del');
+let CHECKBOXES = document.querySelectorAll('.box');
+let taskList = [];
 taskDelete();
 taskAdd();
 checkedStyle();
 
 
-//const taskList = [];
+
 
 
 
@@ -15,27 +16,29 @@ for (let button of BUTTONS_DEL) {
 	button.addEventListener('click', function (event) {
 		console.log('кнопка нажата');
 		const targetTask = event.target;
-		taskName = targetTask.previousSibling.textContent;
-		//taskList = taskList.filter(item => item !== taskName);
+		let taskName = targetTask.previousSibling.textContent;
+		taskList = taskList.filter(item => item !== taskName);
 		targetTask.parentElement.remove();
-		BUTTONS_DEL = document.querySelectorAll('todo__btn--del');
-		INPUTS_CHECKED = document.querySelectorAll('option:checked');
+		BUTTONS_DEL = document.querySelectorAll('.todo__btn--del');
+		CHECKBOXES = document.querySelectorAll('.box');
 	})
 }
 }
 
 function checkedStyle(){
-for (let box of INPUTS_CHECKED) {
+for (let box of CHECKBOXES) {
+	if(box.checked === true) {
 	box.parentNode.style.backgroundColor = "grey";
 	box.style.textDecoration = "overline";
-	INPUTS_CHECKED = document.querySelectorAll('option:checked');
+	CHECKBOXES = document.querySelectorAll('.box');
+	}
 }
 }
 
 function taskAdd(){
-	for (button of TODO_UI.BUTTONS_ADD) {
+	for (let button of TODO_UI.BUTTONS_ADD) {
 		button.addEventListener('click', function (event) {
-      alert('кнопка нажата');
+       
 		})
 
 	}
